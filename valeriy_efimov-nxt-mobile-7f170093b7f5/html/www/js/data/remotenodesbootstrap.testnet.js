da8a7f577b20048b45ed3d7917ee535e59f86528
valeriy.efimov <admin@vkonekte.com>
2018-08-16 09:51:03 +0300

    mobile_nxt

---
+++ valeriy_efimov-nxt-mobile-7f170093b7f5/html/www/js/data/remotenodesbootstrap.testnet.js
@@ -0,0 +1,99 @@
+/******************************************************************************
+ * Copyright © 2013-2016 The Nxt Core Developers.                             *
+ * Copyright © 2016-2018 Jelurida IP B.V.                                     *
+ *                                                                            *
+ * See the LICENSE.txt file at the top-level directory of this distribution   *
+ * for licensing information.                                                 *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement with Jelurida B.V.,*
+ * no part of the Nxt software, including this file, may be copied, modified, *
+ * propagated, or distributed except according to the terms contained in the  *
+ * LICENSE.txt file.                                                          *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+RemoteNodesManager.prototype.REMOTE_NODES_BOOTSTRAP =
+    {
+        "peers": [
+            {
+                "downloadedVolume": 35186073,
+                "address": "178.150.207.53",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 32756158,
+                "services": [
+                    "API",
+                    "API_SSL",
+                    "CORS"
+                ],
+                "version": "1.11.13",
+                "platform": "https://nxt.noip.me:6877/",
+                "inboundWebSocket": true,
+                "apiSSLPort": 6877,
+                "lastUpdated": 142893457,
+                "blacklisted": false,
+                "announcedAddress": "178.150.207.53",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 142886221,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "downloadedVolume": 431618414,
+                "address": "5.200.23.15",
+                "inbound": true,
+                "blockchainState": "DOWNLOADING",
+                "weight": 0,
+                "uploadedVolume": 7052432185,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.12",
+                "platform": "Linux amd64",
+                "inboundWebSocket": false,
+                "lastUpdated": 142896561,
+                "blacklisted": false,
+                "announcedAddress": "5.200.23.15",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 142896561,
+                "state": 1,
+                "shareAddress": true
+            },
+            {
+                "downloadedVolume": 2451171,
+                "address": "188.190.229.97",
+                "inbound": true,
+                "blockchainState": "UP_TO_DATE",
+                "weight": 0,
+                "uploadedVolume": 6725015,
+                "services": [
+                    "API",
+                    "CORS"
+                ],
+                "version": "1.11.12",
+                "platform": "Linux amd64",
+                "inboundWebSocket": true,
+                "lastUpdated": 142894857,
+                "blacklisted": false,
+                "announcedAddress": "188.190.229.97",
+                "apiPort": 6876,
+                "application": "NRS",
+                "port": 6874,
+                "outboundWebSocket": true,
+                "lastConnectAttempt": 142891241,
+                "state": 1,
+                "shareAddress": true
+            }
+        ],
+        "requestProcessingTime": 3
+    };
